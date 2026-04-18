678980e05acd47e3697444b0e8d5cad3304be348
zenithayan7-code <zenithayan7@gmail.com>
2026-04-19 01:02:02 +0600

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
 	version: "1.0.0",
-	credits: "SHAHADAT SAHU",
+	credits: "AYAN KHAN",
 	description: "Record bot activity notifications!",
 	envConfig: {
 		enable: true
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	const logger = require("../../utils/log");
 	if (!global.configModule[this.config.name].enable) return;
 	
-	let formReport = "=== ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
+	let formReport = "=== ─꯭─⃝‌‌AYAN 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
 					"\n\n» Thread ID: " + event.threadID +
 					"\n» Action: {task}" +
 					"\n» Action created by userID: " + event.author +
