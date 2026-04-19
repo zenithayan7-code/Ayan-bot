4b4ac38af07a1169055f8e50e362b7fd3242513c
zenithayan7-code <zenithayan7@gmail.com>
2026-04-19 08:45:28 +0600

    Update baby.js

--- Script/commands/baby.js
+++ Script/commands/baby.js
@@ -7,7 +7,7 @@ module.exports.config = {
  name: "baby",
  version: "1.0.3",
  hasPermssion: 0,
- credits: "ULLASH",
+ credits: "AYAN",
  description: "Cute AI Baby Chatbot | Talk, Teach & Chat with Emotion ☢️",
  commandCategory: "Chat",
  usages: "[message/query]",
